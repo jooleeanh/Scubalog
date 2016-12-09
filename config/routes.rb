5cb5d340cbc8cf6cd931d806422645414f0d1014
Julian Honma <julian.honma@gmail.com>
2016-12-09 14:04:24 +0100

    configure omniauth fb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   root to: 'pages#home'
 end
