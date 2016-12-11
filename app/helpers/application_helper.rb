2d4210b5715cd91583a82bfc2efa0cd187b15400
Julian Honma <julian.honma@gmail.com>
2016-12-11 23:15:29 +0100

    set user picture to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def avatar_url
+    if current_user
+      if current_user.facebook_picture_url != nil
+        current_user.facebook_picture.url
+      elsif current_user.google_picture_url != nil
+        current_user.google_picture_url
+      else
+        "diver_avatar.png"
+      end
+    end
+  end
 end
