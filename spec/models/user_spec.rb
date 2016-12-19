9b9419a10041b0604bb937f294b3b3a9a9b57854
Julian Honma <julian.honma@gmail.com>
2016-12-19 22:19:48 +0100

    add basic tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
+OmniAuth.config.test_mode = true
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:new_user) do
+    User.new(email: "test@test.test",
+             password: "testing",
+             first_name: "tester",
+             last_name: "testers")
+  end
+
+  it "is valid with valid attributes" do
+    expect(new_user).to be_valid
+  end
+
+  it "is not valid without an email" do
+    new_user.email = nil
+    expect(new_user).to_not be_valid
+  end
+
+  it "is not valid without a password with 6 characters or more" do
+    new_user.password = "12345"
+    expect(new_user).to_not be_valid
+  end
+
+  it "is not valid without a first name" do
+    new_user.first_name = nil
+    expect(new_user).to_not be_valid
+  end
+
+  it "is not valid without a last name" do
+    new_user.last_name = nil
+    expect(new_user).to_not be_valid
+  end
+
+  it "is not valid without a avatar picture url" do
+    new_user.avatar_picture_url = nil
+    expect(new_user).to_not be_valid
+  end
+
+  it "can sign up with the sign-up form"
+  it "can sign up with facebook"
+  it "can sign up with google"
 end
