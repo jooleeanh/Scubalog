7ab15ebc49cb1bf0cff9e00cea8d70e3e4d2b054
Julian Honma <julian.honma@gmail.com>
2016-12-20 22:37:42 +0100

    create animal and sighting models

---
+++ spec/models/animal_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Animal, type: :model do
+  it "is valid with valid attributes"
+  it "must have a name"
+  it "name must be unique"
+end
---
+++ spec/models/sighting_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Sighting, type: :model do
+  it "is valid with valid attributes"
+  it "must belong to an animal"
+  it "must belong to a sighteable"
+end
