af4979961cddcd390cdc1cf31b478b2d30f81634
Julian Honma <julian.honma@gmail.com>
2016-12-20 18:51:58 +0100

    add spearfishing category in gear

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,5 +1,6 @@
 class Gear < ApplicationRecord
-  DIVING = %w(mask snorkel fins wetsuit light gloves footwear weightbelt weight)
+  DIVING = %w(mask snorkel fins wetsuit light gloves footwear weightbelt weight computer camera buoy)
   SCUBADIVING = %w(tank bcd regulator drysuit compass undergarment)
   FREEDIVING = %w(noseclip lanyard)
+  SPEARFISHING = %w(speargun)
 end
