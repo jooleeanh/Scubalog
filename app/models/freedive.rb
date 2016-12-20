a1477d1c8ec9ad73439e0ca2d02defabe7c1a2a3
Julian Honma <julian.honma@gmail.com>
2016-12-20 20:12:31 +0100

    Add dive types to freedive and scubadive

--- app/models/freedive.rb
+++ app/models/freedive.rb
@@ -2,4 +2,5 @@ class Freedive < ApplicationRecord
   belongs_to :user
   belongs_to :divespot
   belongs_to :gear_set
+  DIVE_TYPES = %w(Snorkeling Spearfishing CNF CWT FIM VWT NLT STA DYN DNF The\ Jump\ Blue Speed\ Endurance Skandalopetra)
 end
