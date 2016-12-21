db58a5d3a366727bb5515793216c9a06ab3198d5
Julian Honma <julian.honma@gmail.com>
2016-12-21 14:45:10 +0100

    create add_dive_type method for DB array

--- app/models/dive.rb
+++ app/models/dive.rb
@@ -1,3 +1,8 @@
 class Dive < ApplicationRecord
   belongs_to :divable, polymorphic: true
+
+  def add_dive_type(dive_type)
+    dive_types_will_change!
+    update_attributes dive_types: dive_types.push(dive_type)
+  end
 end
