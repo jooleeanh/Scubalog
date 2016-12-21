c910672bbff4565a810e52d906de7eb31694aee4
Julian Honma <julian.honma@gmail.com>
2016-12-21 22:02:44 +0100

    refacto user stats spec

--- spec/models/user/user_stats_spec.rb
+++ spec/models/user/user_stats_spec.rb
@@ -1,131 +1,64 @@
 require 'rails_helper'
 OmniAuth.config.test_mode = true
 
+MODELS = [
+  {model_object: Scubadive, verb: "scubadiving", noun_singular: "scubadive", noun_plural: "scubadives" },
+  {model_object: FreediveSession, verb: "freediving", noun_singular: "freedive", noun_plural: "freedives" },
+  {model_object: Dive, verb: "diving", noun_singular: "dive", noun_plural: "dives" }
+]
+
+LOCATION_FIELDS = [
+  {name: "divespots"},
+  {name: "cities"},
+  {name: "states"},
+  {name: "regions"},
+  {name: "countries"},
+  {name: "continents"},
+]
+
 RSpec.describe User, type: :model do
   context "statistics" do
-    context "time" do
-      describe "#time_underwater_scubadiving" do
-        it "returns total time spent underwater in seconds while scubadiving"
+    MODELS.each do |model_hash|
+      context "#{model_hash[:model_object]}" do
+        context "time" do
+          describe "#time_underwater_#{model_hash[:verb]}" do
+            it "returns total time spent underwater in seconds while #{model_hash[:verb]}"
+          end
+          describe "#dive_count_per_year_#{model_hash[:verb]}" do
+            it "returns a hash of number of #{model_hash[:noun_plural]} per year (sort desc)"
+          end
+        end
+        context "location" do
+          LOCATION_FIELDS.each do |field|
+            describe "#top_divespots_scubadiving" do
+              it "returns a hash of number of #{model_hash[:noun_plural]} per #{field[:name]} (sort desc)"
+            end
+          end
+        end
+        context "records" do
+          unless model_hash[:model_object] == Dive
+            describe "#deepest_dive_#{model_hash[:verb]}" do
+              it "returns the deepest #{model_hash[:noun_singular]}"
+            end
+            describe "#longest_dive_#{model_hash[:verb]}" do
+              it "returns the longest #{model_hash[:noun_singular]}"
+            end
+            describe "#most_dives_in_a_day_#{model_hash[:verb]}" do
+              it "returns the highest number of #{model_hash[:noun_plural]} done in a day"
+            end
+          end
+        end
+        context "other" do
+        describe "#top_dive_types_#{model_hash[:verb]}" do
+          it "returns a hash of number of dive types done while #{model_hash[:verb]} (sort desc)"
+        end
+        describe "#top_buddies_#{model_hash[:verb]}" do
+          it "returns a hash of number of dives with each buddy while #{model_hash[:verb]} (sort desc)"
+        end
+        describe "#top_sightings_#{model_hash[:verb]}" do
+          it "returns a hash of number of animal sightings while #{model_hash[:verb]} (sort desc)"
+        end
       end
-      describe "#time_underwater_freediving" do
-        it "returns total time spent underwater in seconds while freediving"
-      end
-      describe "#time_underwater_diving" do
-        it "returns total time spent underwater in seconds while (scuba|free)diving"
-      end
-      describe "#dive_count_per_year_scubadiving" do
-        it "returns a hash of number of scubadives per year (sort desc)"
-      end
-      describe "#dive_count_per_year_freediving" do
-        it "returns a hash of number of freedives per year (sort desc)"
-      end
-      describe "#dive_count_per_year_diving" do
-        it "returns a hash of number of (scuba|free)dives per year (sort desc)"
-      end
-    end
-    context "location" do
-      describe "#top_divespots_scubadiving" do
-        it "returns a hash of divespots done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_divespots_freediving" do
-        it "returns a hash of divespots done while freediving sorted by count (descending)"
-      end
-      describe "#top_divespots_diving" do
-        it "returns a hash of divespots done while (scuba|free)diving sorted by count (descending)"
-      end
-      describe "#top_cities_scubadiving" do
-        it "returns a hash of cities done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_cities_freediving" do
-        it "returns a hash of cities done while freediving sorted by count (descending)"
-      end
-      describe "#top_cities_diving" do
-        it "returns a hash of cities done while (scuba|free)diving sorted by count (descending)"
-      end
-      describe "#top_states_scubadiving" do
-        it "returns a hash of states done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_states_freediving" do
-        it "returns a hash of states done while freediving sorted by count (descending)"
-      end
-      describe "#top_states_diving" do
-        it "returns a hash of states done while (scuba|free)diving sorted by count (descending)"
-      end
-      describe "#top_regions_scubadiving" do
-        it "returns a hash of regions done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_regions_freediving" do
-        it "returns a hash of regions done while freediving sorted by count (descending)"
-      end
-      describe "#top_regions_diving" do
-        it "returns a hash of regions done while (scuba|free)diving sorted by count (descending)"
-      end
-      describe "#top_countries_scubadiving" do
-        it "returns a hash of countries done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_countries_freediving" do
-        it "returns a hash of countries done while freediving sorted by count (descending)"
-      end
-      describe "#top_countries_diving" do
-        it "returns a hash of countries done while (scuba|free)diving sorted by count (descending)"
-      end
-      describe "#top_continents_scubadiving" do
-        it "returns a hash of continents done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_continents_freediving" do
-        it "returns a hash of continents done while freediving sorted by count (descending)"
-      end
-      describe "#top_continents_diving" do
-        it "returns a hash of continents done while (scuba|free)diving sorted by count (descending)"
-      end
-    end
-    context "records" do
-      describe "#deepest_dive_scubadiving" do
-        it "returns the deepest scubadive"
-      end
-      describe "#deepest_dive_freediving" do
-        it "returns the deepest freedive"
-      end
-      describe "#longest_dive_scubadiving" do
-        it "returns the longest scubadive"
-      end
-      describe "#longest_dive_freediving" do
-        it "returns the longest freedive"
-      end
-      describe "#most_dives_in_a_day_scubadiving" do
-        it "returns the highest number of scubadives done in a day"
-      end
-      describe "#most_dives_in_a_day_freediving" do
-        it "returns the highest number of freedives done in a day"
-      end
-    end
-    context "other" do
-      describe "#top_dive_types_scubadiving" do
-        it "returns a hash of dive types done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_dive_types_freediving" do
-        it "returns a hash of dive types done while freediving sorted by count (descending)"
-      end
-      describe "#top_dive_types_diving" do
-        it "returns a hash of dive types done while (scuba|free)diving sorted by count (descending)"
-      end
-      describe "#top_buddies_scubadiving" do
-        it "returns a hash of buddies done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_buddies_freediving" do
-        it "returns a hash of buddies done while freediving sorted by count (descending)"
-      end
-      describe "#top_buddies_diving" do
-        it "returns a hash of buddies done while (scuba|free)diving sorted by count (descending)"
-      end
-      describe "#top_sightings_scubadiving" do
-        it "returns a hash of sightings done while scubadiving sorted by count (descending)"
-      end
-      describe "#top_sightings_freediving" do
-        it "returns a hash of sightings done while freediving sorted by count (descending)"
-      end
-      describe "#top_sightings_diving" do
-        it "returns a hash of sightings done while (scuba|free)diving sorted by count (descending)"
       end
     end
   end
