bce9660f94d546d29362215d7e9515b64ad0f7f7
Julian Honma <julian.honma@gmail.com>
2016-12-24 15:06:42 +0100

    clean up and organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,4 @@
 Rails.application.routes.draw do
-  resources :sightings
-  resources :scubadives
-  resources :gears
-  resources :gear_uses
-  resources :gear_sets
-  resources :freedive_sessions
-  resources :divespots
-  resources :dives
-  resources :buddies
-  resources :animals
-  get 'dive/index'
-
-  get 'dive/show'
-
-  get 'dive/new'
-
-  get 'dive/create'
-
-  get 'dive/edit'
-
-  get 'dive/update'
-
-  get 'dive/delete'
-
-  get 'gear_set/index'
-
-  get 'gear_set/show'
-
-  get 'gear_set/new'
-
-  get 'gear_set/edit'
-
-  get 'gear/index'
-
-  get 'gear/show'
-
-  get 'gear/new'
-
-  get 'gear/edit'
-
-  get 'freedive_session/index'
-
-  get 'freedive_session/show'
-
-  get 'freedive_session/new'
-
-  get 'freedive_session/edit'
-
-  get 'scubadive/index'
-
-  get 'scubadive/show'
-
-  get 'scubadive/new'
-
-  get 'scubadive/edit'
-
-  get 'divespot/index'
-
-  get 'divespot/show'
 
   root to: 'pages#home'
   devise_for :users, controllers: {
@@ -66,4 +7,23 @@ Rails.application.routes.draw do
   }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  resources :divespots
+  resources :animals
+  resources :gears
+
+  # 'nested' in current_user:
+  resources :scubadives do
+    resources :dives
+    resources :buddies
+    resources :sightings
+  end
+  resources :freedive_sessions do
+    resources :dives
+    resources :buddies
+    resources :sightings
+  end
+  resources :gear_sets do
+    resources :gear_uses
+  end
 end
