b1ff8b637080bc57d0e86d8a10e09d9c56343bfa
Julian Honma <julian.honma@gmail.com>
2016-12-24 14:01:24 +0100

    modify gender type directly in first migration

--- db/migrate/20161211215450_add_fields_to_users.rb
+++ db/migrate/20161211215450_add_fields_to_users.rb
@@ -1,6 +1,6 @@
 class AddFieldsToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :dob, :date
-    add_column :users, :gender, :string
+    add_column :users, :gender, :integer
   end
 end
